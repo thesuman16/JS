e075082431667aaace77f48681c465e3d0d6eedb
suman160g <suman160g@gmail.com>
2024-06-18 06:41:34 +0000

    nix

--- Part1/LetConstVar.js
+++ Part1/LetConstVar.js
@@ -1,8 +1,9 @@
 const accountId = 16638 //not changable and only for constant
+
 let accountEmail = "Suman@gmail.com" //changable we use let in modern script
 var accountPass = "123445" // use in old days // discontinue for issue in block and functional scope
 accountCity = "Jaipur" //
-let accountState //undefined
+let accountState //undefined 
 
 // accountId = 2 //not allowed
 accountEmail = "suman@speedmodi.com"
